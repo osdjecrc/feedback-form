1c7b3e266b889f4d5f3ebe9188170352b1e1198e
Laveesh Gupta <loveeshgoyal@gmail.com>
2019-02-14 12:01:44 +0530

    form handler fixed

--- js/main.js
+++ js/main.js
@@ -1,94 +1,95 @@
+(function($) {
+  'use strict';
 
-(function ($) {
-    "use strict";
+  let isFormValid = true;
 
-    /*==================================================================
+  /*==================================================================
     [ Focus Contact2 ]*/
-    $('.input100').each(function(){
-        $(this).on('blur', function(){
-            if($(this).val().trim() != "") {
-                $(this).addClass('has-val');
-            }
-            else {
-                $(this).removeClass('has-val');
-            }
-        })    
-    })
+  $('.input100').each(function() {
+    $(this).on('blur', function() {
+      if (
+        $(this)
+          .val()
+          .trim() != ''
+      ) {
+        $(this).addClass('has-val');
+      } else {
+        $(this).removeClass('has-val');
+      }
+    });
+  });
 
-    /*==================================================================
+  /*==================================================================
     [ Validate ]*/
-    var input = $('.validate-input .input100');
+  // var input = $('.validate-input .input100');
 
-    $('.validate-form').on('submit',function(){
-        var check = true;
+  // $('.validate-form').on('submit', function() {
+  //   var check = true;
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
-                showValidate(input[i]);
-                check=false;
-            }
-        }
+  //   for (var i = 0; i < input.length; i++) {
+  //     if (validate(input[i]) == false) {
+  //       showValidate(input[i]);
+  //       if (isFormValid) {
+  //         isValid = false;
+  //       }
+  //       check = false;
+  //     }
+  //   }
 
-        return check;
-    });
+  //   return check;
+  // });
 
+  // $('.validate-form .input100').each(function() {
+  //   $(this).focus(function() {
+  //     hideValidate(this);
+  //   });
+  // });
 
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-        });
-    });
-
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-                return false;
-            }
-        }
-
-        if($(input).attr('type') == 'radio' || $(input).attr('name') == 'radio') {
-
-            var radios = document.getElementsByName("radio");
-            var formValid = false;
-    
-        var i = 0;
-        while (!formValid && i < radios.length) {
-            if (radios[i].checked) formValid = true;
-            i++;        
-        }
-            if(!formValid) {
-                return false;
-            }
-        }
-        else {
-            if($(input).val().trim() == ''){
-                return false;
-            }
-        }
+  function validate(input) {
+    if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+      if (
+        $(input)
+          .val()
+          .trim()
+          .match(
+            /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/
+          ) == null
+      ) {
+        return false;
+      }
+    } else {
+      if (
+        $(input)
+          .val()
+          .trim() == ''
+      ) {
+        return false;
+      }
     }
+  }
 
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
+  function showValidate(input) {
+    var thisAlert = $(input).parent();
 
-        $(thisAlert).addClass('alert-validate');
-    }
+    $(thisAlert).addClass('alert-validate');
+  }
 
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
+  function hideValidate(input) {
+    var thisAlert = $(input).parent();
 
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
+    $(thisAlert).removeClass('alert-validate');
+  }
 
-    /*==================================================================
+  /*==================================================================
     [ Show / hide Form ]*/
-    
-    $('.contact100-btn-hide').on('click', function(){
-        $('.wrap-contact100').fadeOut(400);
-    })
 
-    $('.contact100-btn-show').on('click', function(){
-        $('.wrap-contact100').fadeIn(400);
-    })
+  $('.contact100-btn-hide').on('click', function() {
+    $('.wrap-contact100').fadeOut(400);
+  });
 
+  $('.contact100-btn-show').on('click', function() {
+    $('.wrap-contact100').fadeIn(400);
+  });
+
+  return isFormValid;
 })(jQuery);
